02a81275380e34f3674843344e42f4b164c0093d
Chris Anselmo <canselmo@hybridhealth.com>
2017-09-08 12:11:45 -0700

    Display switch functionality added

---
+++ index.js
@@ -0,0 +1,44 @@
+const package = require('./package.json');
+const exec = require('child_process').exec
+
+const Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("mac-display", "DisplaySwitch", macDisplay);
+};
+ 
+function macDisplay(log, config) {
+  this.log = log;
+}
+
+macDisplay.prototype.getServices = function() {
+  let informationService = new Service.AccessoryInformation();
+  informationService
+    .setCharacteristic(Characteristic.Manufacturer, "christopherwk210")
+    .setCharacteristic(Characteristic.Model, "DisplaySwitch")
+    .setCharacteristic(Characteristic.SerialNumber, package.version);
+
+  let switchService = new Service.Switch("DisplaySwitch");
+  switchService
+    .getCharacteristic(Characteristic.On)
+    .on('set', this.setSwitchOnCharacteristic.bind(this))
+    .on('get', this.getSwitchOnCharacteristic.bind(this))
+
+  this.informationService = informationService;
+  this.switchService = switchService;
+  return [informationService, switchService];
+}
+
+macDisplay.prototype.getSwitchOnCharacteristic = function(next) {
+  exec('pmset -g powerstate IODisplayWrangler | tail -1 | cut -c29', (err, stdout, stderr) => {
+    next(parseInt(stdout) < 4);
+  });
+}
+ 
+macDisplay.prototype.setSwitchOnCharacteristic = function(on, next) {
+  this.log('Mac display: ' + on);
+  on ? exec('caffeinate -u -t 1') : exec('pmset displaysleepnow');
+  next();
+}
